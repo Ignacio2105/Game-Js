702e4a6dbae339e8be52acb9af2fd6312476a727
Ignacio C <ignacio.caldsil@gmail.com>
2025-01-29 17:23:17 -0500

    modificación de estilos y game

--- game.js
+++ game.js
@@ -1,33 +1,41 @@
-/*REGLAS 
-- Es necesario primero definir los estilos luego colocar el texto
+const canvas = document.querySelector('#game');
+const game = canvas.getContext('2d');
 
+let canvasSize;
+let elementsSize;
 
-*/
-const canvas = document.querySelector('canvas');
-const game = canvas.getContext('2d'); //Le decimos que es un juego bidimensional
+window.addEventListener('load', setCanvasSize);
+window.addEventListener('resize', setCanvasSize);
 
-//El juego inicializar apenas termine de cargar la pagina
-window.addEventListener('load',startGame); 
+function setCanvasSize() {
+  if (window.innerHeight > window.innerWidth) {
+    canvasSize = window.innerWidth * 0.8;
+  } else {
+    canvasSize = window.innerHeight * 0.8;
+  }
 
-//Funcion para inicializar juego
-function startGame(){
-    let canvasSize;
-    if(window.innerWidth< window.innerHeight){
-        canvasSize= window.innerWidth *0.8;
+  canvas.setAttribute('width', canvasSize);
+  canvas.setAttribute('height', canvasSize);
+
+  elementsSize = canvasSize / 10;
+
+  startGame();
+}
+
+function startGame() {
+  console.log({ canvasSize, elementsSize });
+
+  game.font = elementsSize + 'px Verdana';
+  game.textAlign = 'end';
+
+  const map = maps[0];
+  const mapRows = map.trim().split('\n');
+  const mapRowCols = mapRows.map(row => row.trim().split(''));
+  console.log({ map, mapRows, mapRowCols });
+
+  for (let row = 1; row <= 10; row++) {
+    for (let col = 1; col <= 10; col++) {
+      game.fillText(emojis[mapRowCols[row - 1][col - 1]], elementsSize * col, elementsSize * row);
     }
-    else{
-        canvasSize = window.innerHeight*0.8;
-    }
-
-    canvas.setAttribute('width',canvasSize);
-    canvas.setAttribute('height',canvasSize);
-
-    /*
-    game.fillRect(0,0,100,100); //Creamos un rectangulo de 100x100px
-    game.clearRect(0,0, 100,50);//Borra una parte del rectangulo de 100x50px
-    game.font = '20px Verdana'; //Cambia el tamaño de la fuente
-    game.fillStyle = 'red'; //Cambia el color de la fuente
-    game.textAlign = 'left'; //Alinea el texto 
-    game.fillText('Jueguito Js',20,20);//Escribe texto segun coordenadas
-    */
+  }
 }
